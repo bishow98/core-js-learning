9978298f4e582ea5c8a76c80afc1fd5ad299788b
Bishow Chauhan <chauhandavid4199@gmail.com>
2024-09-23 13:03:09 +0545

    Add examples of using symbols as object keys

--- corejs/day1_datatype_ecma.js
+++ corejs/day1_datatype_ecma.js
@@ -6,4 +6,16 @@ let sym3 = Symbol('description');
 console.log(sym1 === sym2); // false (unique symbols)
 console.log(sym2 === sym3); // false (even with the same description, they are unique)
 
+//using symbol as an object key 
+let mySymbol = Symbol('uniqueKey');
+
+let obj = {
+  [mySymbol]: 'This is a value'
+};
+
+console.log(obj[mySymbol]); // Output: 'This is a value'
+
+// The symbol key is not enumerable
+console.log(Object.keys(obj)); // Output: [] (Symbol keys won't show up here)
+console.log(Object.getOwnPropertySymbols(obj)); // Output: [ Symbol(uniqueKey) ]
 
