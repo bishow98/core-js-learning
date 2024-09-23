ca47641328fca1b53a36c6a3adedfd7bcd4a8c17
Bishow Chauhan <chauhandavid4199@gmail.com>
2024-09-23 13:21:31 +0545

    Convert data types in JavaScript

---
+++ corejs/day1_datatypeconversion.js
@@ -0,0 +1,28 @@
+/*let age = 23
+let score = "40abc"
+
+console.log(typeof(age))
+console.log(typeof(score))
+
+
+let valueInNumber= Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+*/
+
+// '33' => converted  to 33
+// "22abc" => NaN (Not a Number) but type of ma chai number nai aayo
+//true = 1 , false = 0
+
+let isLoggedIn = "";
+let boleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(boleanIsLoggedIn);
+
+//"" empty string means false and "content" this string is true
+
+let numbers = 45;
+let stringNumber = String(numbers);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//herda ta number nai dekhinxa tara typeof use garera check garney ho vaney it is converted to string 
