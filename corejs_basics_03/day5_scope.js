8e2e1fe32532ec22013f25a3f49baea1e38d273e
Bishow Chauhan <chauhandavid4199@gmail.com>
2024-09-27 14:10:30 +0545

    Add day5_scope.js to demonstrate global and local scope in JavaScript

---
+++ corejs_basics_03/day5_scope.js
@@ -0,0 +1,15 @@
+//day 5 scope . Global scope and Local scope 
+// var c = 3232
+let a = 443
+
+if(true){
+    let a = 3
+    const b = 32
+    // var c = 43
+
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c) //block scope ma defined gareko xa tara pani bahira access bhairaxa 
+console.log(a)//global scope bata aako a lai print garxa block scope bata aako lai yesle chindaina 
