4954b4ed660aa4f199a2bee66682ea168c3a94d1
Bishow Chauhan <chauhandavid4199@gmail.com>
2024-09-27 22:14:38 +0545

    Add .gitignore file to exclude /portfolio/ directory Refactor day5_scope.js: Add nested scope example and explain function declaration types

--- corejs_basics_03/day5_scope.js
+++ corejs_basics_03/day5_scope.js
@@ -12,4 +12,39 @@ if(true){
 // console.log(a)
 // console.log(b)
 // console.log(c) //block scope ma defined gareko xa tara pani bahira access bhairaxa 
-console.log(a)//global scope bata aako a lai print garxa block scope bata aako lai yesle chindaina 
+// console.log(a)//global scope bata aako a lai print garxa block scope bata aako lai yesle chindaina 
+
+//nested scope 
+
+function one(){
+    const username= "bishow"
+
+    function two(){
+        const website = "facebook"
+        // console.log(username);
+    }
+
+    // console.log(website)
+    two()
+}
+
+one()
+
+
+// interesting concept of two types of declaration of function
+
+console.log(addone(7))
+function addone(num){ //using direct function method we can call the function at the top as well like 
+    return num + 1
+}
+
+// addone(5)
+
+
+
+// addTwo(6)
+const addTwo = function(num){//while holding the function in variable we cannot call the function at the top as it throw the error : referenceError ->cannot access addTwo before initialization
+    return num + 2
+}
+
+addTwo(8)
